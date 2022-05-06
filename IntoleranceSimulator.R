06461aab6414c5809566b462710b714681f7d537
Loïc Thibaut <Loic.Thibaut@populationgenomics.org.au>
2022-05-06 12:15:16 +1000

    Update to o/e simulator (#11)
    * test
    
    * test code review again
    
    * First draft of o/e simulator
    
    * o/e simulator: updated comments ,added exponential kernel

--- IntoleranceSimulator.R
+++ IntoleranceSimulator.R
@@ -14,8 +14,8 @@ require(bio3d)
 require(MASS)
 
 # protein structure file
-# hard-coded for quick prototyping
 
+# hard-coded for quick prototyping
 
 protfile = 'structures/AF-P68133-F1-model_v2.pdb'
 
@@ -24,13 +24,20 @@ pdb = read.pdb(protfile)
 # calculate residues pairwise distance matrix
 distmat = dm(pdb)
 
-# is upper triangular with empty diagonal: add diagonal, fill lower triangle, convert to matrix
+# distmat is upper triangular with empty diagonal: add diagonal, fill lower triangle, convert to matrix
 dismat = as.matrix(distmat)
 distmat[is.na(distmat)] = 0
 distmat = distmat + t(distmat)
 
 # spatial covariance function: use a Gaussian kernel for prototyping
-sigma = exp(-0.5 * distmat^2)
+
+l=1
+sigma = exp(-0.5 * (distmat/l)^2)
+
+# exponential kernel
+l=1
+sigma = exp(-0.5 * distmat / l)
+
 
 # check that sigma is positive definite
 if(min(eigen(sigma)$values) < 0) stop('Spatial covariance matrix is not positive definite')
@@ -38,13 +45,19 @@ if(min(eigen(sigma)$values) < 0) stop('Spatial covariance matrix is not positive
 # generate a multivariate Gaussian 
 obs = rmvnorm(1, Sigma=sigma)
 
-# use a copula to generate multivariate beta observation
+
+# use a copula to generate correlated multivariate beta observations
+
 # hyper parameters: values matching the empirical distribution of o/e in gnomAD should be used
 # here we fix an arbitrary beta to control the variance and calculate alpha such as the expected value is 0.4
 beta = 5
 m = 0.4
 alpha = beta * m / (1-m)
 obs = qbeta(pnorm(obs), shape1 = alpha, shape2 = beta)
+
+
+# set score on 0-100 range
+obs = round(obs * 100)
 
 # output a 2 columns file for iCn3D
 write.table(t(obs), file='structures/AF-P68133-F1-model_v2.simoe.tsv', sep='\t',
